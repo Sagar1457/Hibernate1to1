5cf114a621ead61b9a33d77bd325df82ea791f46
vidhy <vidhy@BVS-PC>
2023-09-25 16:27:26 +0530

    one to one

---
+++ src/main/java/com/example/lab10/App.java
@@ -0,0 +1,43 @@
+package com.example.lab10;
+//BVIDHYASAGAR..AF0320724
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.example.lab10.model.Passport;
+import com.example.lab10.model.Person;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            try
+            {
+            	Person user=new Person();
+            	user.setPerson_name("sagar");
+            	
+            	Passport details = new Passport();
+            	details.setDetails("yes");
+            	
+            	user.setDetails(details);
+            	details.setUser(user);
+            	
+            	session.save(user);
+            	session.getTransaction().commit();
+            	
+            	Person retrievedUser=session.get(Person.class, user.getId());
+       		    Passport retrievedPassport=retrievedUser.getDetails();
+       		    
+       		    System.out.println("User: "+retrievedUser.getPerson_name());
+       		    System.out.println("Passport: "+retrievedPassport.getDetails());
+
+            	
+            }
+            finally 
+    		{
+    			session.close();
+                sessionFactory.close();
+    		}
+    }
+}
